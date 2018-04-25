595890150bf1385ba8096c95579b70cb96101451
festigf <gianfranco.festi@marconirofereto.it>
2018-04-25 09:10:03 +0200

    Inizio

---
+++ src/main/java/com/example/SbaseApplication.java
@@ -0,0 +1,14 @@
+package com.example;
+
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+
+@SpringBootApplication
+public class SbaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbaseApplication.class, args);
+	}
+}
